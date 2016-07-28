f43d4d641e2486d32481d45c784601a239bc7dfc
Kiwi <kevinkreuzer@Kevins-MacBook-Pro.local>
2016-07-28 19:36:02 +0200

    Leafnode and Rx Included and First map displayed

--- app/main.js
+++ app/main.js
@@ -1,3 +1,12 @@
-setTimeout(() => {
-  document.getElementsByTagName('h1')[0].innerHTML = "Hallo";
-}, 1000);
+var QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp';
+
+loadJSONP = (url) => {
+  let script = documenct.createElement('script');
+  script.src = url;
+
+  let head = document.getElementsByTagName('head')[0];
+  head.appendChild(script);
+}
+
+let map = L.map('map').setView([33.858631, -118.279602], 7);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
