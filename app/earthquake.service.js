a42176b27210e319a4dc61713d2d9e46174df83f
Kiwi <kevinkreuzer@Kevins-MacBook-Pro.local>
2016-07-29 16:11:46 +0200

    Make the app displaying real time data

--- app/earthquake.service.js
+++ app/earthquake.service.js
@@ -1,17 +1,22 @@
 let QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp';
 
-
-let quakes = Rx.DOM.jsonpRequest({
+let earthQuakeData$ = Rx.DOM.jsonpRequest({
   url: QUAKE_URL,
   jsonpCallback: 'eqfeed_callback'
 })
-.flatMap((dataset) => {
-  return Rx.Observable.from(dataset.response.features);
-})
-.map((quake) => ({
-  lat: quake.geometry.coordinates[1],
-  lng: quake.geometry.coordinates[0],
-  size: quake.properties.mag * 10000
+
+let quakes = Rx.Observable
+  .interval(5000)
+  .flatMap(() => earthQuakeData$)
+  .retry(3)
+  .flatMap((dataset) => {
+    return Rx.Observable.from(dataset.response.features);
+  })
+  .distinct((quake) => quake.properties.code)
+  .map((quake) => ({
+    lat: quake.geometry.coordinates[1],
+    lng: quake.geometry.coordinates[0],
+    size: quake.properties.mag * 10000
   })
 );
 
