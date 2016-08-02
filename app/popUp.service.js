b5a844ee868eba7a60f63aea88100c402cf26e05
Kiwi <kevinkreuzer@Kevins-MacBook-Pro.local>
2016-08-02 10:02:39 +0200

    Optimize the format of the date

--- app/popUp.service.js
+++ app/popUp.service.js
@@ -12,17 +12,22 @@ let mouseMoves$ = Rx.Observable.fromEvent(map, 'click')
   });
 
 mouseMoves$.subscribe((event) => {
-  console.log('ich logge', event);
   let latlng = {
     lat: event.lat,
     lng: event.lng
   }
 
   let date = new Date(1324339200000);
-  let time = date.getHours() + ':' + date.getMinutes();
+  let time = convertToTimeString(date);
 
   L.popup()
     .setLatLng(latlng)
     .setContent('<p><h4>' + event.place + '</h4><ul><li>' + event.type + '</li><li>' + time + '</li></ul></p>')
     .openOn(map);
 });
+
+convertToTimeString = (date) => {
+  let hoursString = date.getHours().toString().length === 1 ? '0' + date.getHours() : date.getHours();
+  let minutesString = date.getMinutes().toString().length === 1 ? '0' + date.getMinutes() : date.getMinutes();
+  return hoursString + ':' + minutesString;
+}
